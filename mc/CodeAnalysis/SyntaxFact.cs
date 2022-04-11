a8628d89d2e9d00bccaa32ebc4d4f103d43f5e03
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-04-11 15:05:07 +0200

    Aggiunta  di UnaryOperator nell Programm

--- mc/CodeAnalysis/SyntaxFact.cs
+++ mc/CodeAnalysis/SyntaxFact.cs
@@ -16,5 +16,15 @@ namespace Minsk.CodeAnalysis {
 					return 0;
 			}
 		}
+
+		public static int GetUnaryOperatorPrecedence(this SyntaxKind kind){
+			switch (kind) {
+				case SyntaxKind.PlusToken:
+				case SyntaxKind.MinusToken:
+					return 3;
+				default:
+					return 0;
+			}
+		}
 	}
 }
